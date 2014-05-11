b492ca7f5a8a25d0c61ecc29932c32315e6ae95b
Patrick McVeety-Mill <p.mcvtm@gmail.com>
2014-05-11 12:05:29 -0500

    added test project and simple collision tester

---
+++ src/Tests/Collisions/CircleCollisionTests.cs
@@ -0,0 +1,31 @@
+﻿using Should;
+using Warg;
+
+namespace Tests.Collisions
+{
+	public class CircleCollisionTests
+	{
+		public void ShouldCollideWhenOverlapping()
+		{
+			var circle1 = new TestCircle(10, 10, 5);
+			var circle2 = new TestCircle(15, 15, 6);
+
+			circle1.IsCollidingWith(circle2).ShouldBeTrue();
+		}
+
+		public void ShouldCollideWhenIdentical()
+		{
+			var circle = new TestCircle(10, 10, 5);
+
+			circle.IsCollidingWith(circle).ShouldBeTrue();
+		}
+
+		public void ShouldNotCollideWhenNotOverlapping()
+		{
+			var circle1 = new TestCircle(10, 10, 5);
+			var circle2 = new TestCircle(20, 20, 2);
+
+			circle1.IsCollidingWith(circle2).ShouldBeFalse();
+		}
+	}
+}
---
+++ src/Tests/Collisions/TestCircle.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework;
+using Warg;
+
+namespace Tests.Collisions
+{
+	public class TestCircle : ICircleCollider
+	{
+
+		public TestCircle(float centerX, float centerY, int radius)
+		{
+			Center = new Vector2(centerX, centerY);
+			Radius = radius;
+		}
+
+		public Vector2 Center { get; set; }
+		public int Radius { get; set; }
+	}
+}
